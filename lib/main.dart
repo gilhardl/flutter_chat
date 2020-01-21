bcec32204d9dcca91e4c02603efd2565d92217e4
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-22 00:46:58 +0100

    move chat screen implementation under the ChatScreen class

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,16 @@ class FriendlychatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Friendlychat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Friendlychat"),
-        ),
-      ),
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
     );
   }
 }
