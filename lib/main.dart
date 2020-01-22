dff15464a50f9e87d5c5b06d7c239c17c33e1b20
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-22 15:57:49 +0100

    add message composer widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,33 @@ class FriendlychatApp extends StatelessWidget {
 }
 
 class ChatScreen extends StatefulWidget {
-
   @override
   State createState() => ChatScreenState();
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat")),
+      body: _buildMessageComposer()
     );
+  }
+
+  Widget _buildMessageComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: TextField(
+        controller: _textCtrl,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(hintText: "Send a message")
+      )
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textCtrl.clear();
   }
 }
