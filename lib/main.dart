f503bf8f5249d41df3a2620883714b8fd65e741a
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-22 15:12:22 +0100

    ChatScreen becomes stateful

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,13 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
