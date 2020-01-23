ba991a0c65bac0a5892ee515498e4cd52eff85c4
Lucas GILHARD <l.gilhard@gmail.com>
2020-01-23 13:40:33 +0100

    add messages list UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// Hard-coded value for simplicity but in real apps we'll retrieve the sender's name via authentication
+const String _name = "Your Name";
+
 void main() {
   runApp(FriendlychatApp());
 }
@@ -20,13 +23,31 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(title: new Text("Friendlychat")),
-        body: _buildMessageComposer());
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              ),
+            ),
+            Divider(
+              height: 1,
+            ),
+            Container(
+                decoration: BoxDecoration(color: Theme.of(context).cardColor),
+                child: _buildMessageComposer()),
+          ],
+        ));
   }
 
   Widget _buildMessageComposer() {
@@ -57,5 +78,40 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textCtrl.clear();
+    ChatMessage message = ChatMessage(text: text);
+    setState(() {
+      _messages.add(message);
+    });
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
